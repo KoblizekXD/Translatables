1e8ccf1d32b6bb426f42f452d6879cb49a2d7ee5
KoblizekXD <janprokupek@centrum.cz>
2022-10-20 09:54:34 +0200

    Main class + language class update

---
+++ src/main/java/com/koblizek/translatables/Translations.java
@@ -0,0 +1,41 @@
+package com.koblizek.translatables;
+
+import com.koblizek.translatables.lang.Language;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Translations {
+    private static Language language;
+
+    private Translations() {}
+
+    public static List<Language> loadLanguages(String directory) {
+        File dir = new File(directory);
+        if (!dir.isDirectory() || !dir.exists())
+            throw new RuntimeException("Directory either not exists or it's not directory");
+        File[] jsonFiles = dir.listFiles(f -> f.isFile() && f.getName().endsWith(".json"));
+        if (jsonFiles == null)
+            throw new RuntimeException("Path doesn't exists or IO error occurred");
+        List<Language> languages = new ArrayList<>();
+        for (File jsonFile : jsonFiles) {
+            StringBuilder content = new StringBuilder();
+            try (BufferedReader r = new BufferedReader(new FileReader(jsonFile));) {
+                while (r.ready()) {
+                    content.append(r.readLine());
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            languages.add(new Language(jsonFile, content.toString()));
+        }
+        return languages;
+    }
+    public static void setCurrentLanguage(Language language) {
+        Translations.language = language;
+    }
+    public static Language getCurrentLanguage() {
+        return Translations.language;
+    }
+}
---
+++ src/main/java/com/koblizek/translatables/lang/Language.java
@@ -0,0 +1,9 @@
+package com.koblizek.translatables.lang;
+
+import java.io.File;
+
+public class Language {
+    public Language(File file, String content) {
+
+    }
+}
