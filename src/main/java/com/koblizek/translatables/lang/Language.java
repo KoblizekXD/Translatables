f7e02f2b4a154bc21cd592985cddbfd85abaad8e
KoblizekXD <janprokupek@centrum.cz>
2022-10-20 10:01:06 +0200

    You can now get stuff from language pack

--- src/main/java/com/koblizek/translatables/lang/Language.java
+++ src/main/java/com/koblizek/translatables/lang/Language.java
@@ -1,9 +1,25 @@
 package com.koblizek.translatables.lang;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
 import java.io.File;
 
 public class Language {
-    public Language(File file, String content) {
+    @Getter
+    private final File file;
+    @Getter
+    private final String content;
+    @Getter
+    private final JsonObject json;
 
+    public Language(File file, String content) {
+        this.file = file;
+        this.content = content;
+        this.json = new Gson().fromJson(content, JsonObject.class);
+    }
+    public String getFromKey(String key) {
+        return json.get(key).getAsString();
     }
 }
